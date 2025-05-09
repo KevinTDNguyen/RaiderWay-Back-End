91987f1d481fa2f86de068a143964f05008fa6c9
Kevin <kevintandatnguyen@gmail.com>
2025-05-08 21:12:30 -0400

    JSON Files Creation

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import json
 from Course import Course
 
+"""
+Below was the ancient way of initializing prerequisites and courses
+"""
 # math1 = Course("Math1", "MTH1")
 # math2 = Course("Math2", "MTH2")
 # math3A = Course("Math3A", "MTH3A")
@@ -15,9 +18,36 @@ from Course import Course
 
 # math4.printPrerequisite()
 
-with open("mathcourses.json", "r") as file:
-    data = json.load(file) #turns the JSON data into a python dictionary
+"""
+Revised way of initializing and setting prerequisites
+"""
+allCourses = dict() # init dictionary w/ course objects
 
-for course in data:
-    print(course["name"], course["code"])
+with open("proof.json", "r") as file:
+    course_data = json.load(file) #turns all json data in one dictionary
+
+for course in course_data: #this loop initiates all objects
+    allCourses[course["code"]] = Course(course["name"], course["code"])
+
+#inits prereqs
+for jsonObj in course_data:
+    course = allCourses[jsonObj["code"]] #gets the current course object in the allCourses dictionary using the obj["code"] as a key
+    for prereqCode in jsonObj["prereq"]:
+        prereqObj = allCourses[prereqCode] #redundant for clarity
+        course.setPrerequisite(prereqObj)
+
+for course in allCourses:
+    prereqStr = ""
+    for prereq in allCourses[course].prerequisite:
+        prereqStr += prereq.courseCode + " "
+    print(course + ": " + prereqStr)
+
+"""
+to quickly view the contents of the dictionary copy & paste the loop below
+"""
+# for item in allCourses:
+#     print(item + ": " + allCourses[item].name)
+
+
+
 
